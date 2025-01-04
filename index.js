e8ae008dcf7e15a8f9aaa03d1a048e05e3885e56
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2025-01-04 19:51:27 +0600

    registration and login

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const router = require("./routers");
+const cors = require('cors')
 const dbConnect = require("./config/dbConfig");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dbConnect();
 app.use(router);
+app.use(cors())
 
 app.listen(process.env.SERVER_PORT || 8080, () => {
   console.log("Server is running");
