254a0d8a4247922dcca99ef8407c34dbac54c1ee
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2025-01-25 17:45:29 +0600

    image uploads

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 async function CreateCategory(req, res) {
   let { name, image, description } = req.body;
+  console.log(req.file)
   res.send(req.body);
 }
 
--- routers/api/category.js
+++ routers/api/category.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const { CreateCategory } = require("../../controllers/categoryController");
-const multer  = require('multer')
+const multer = require("multer");
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
 
-router.post("/createcategory" , upload.single('profile'), CreateCategory)
+    const extention = file.originalname.split(".");
+
+    cb(null, file.fieldname + "-" + uniqueSuffix + `.${extention[1]}`);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.post("/createcategory", upload.single("profile"), CreateCategory);
 module.exports = router;
