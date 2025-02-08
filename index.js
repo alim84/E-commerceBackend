c323253750605e31d75c43bed0ae558783443fb8
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2025-02-08 13:07:41 +0600

    category update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 const router = require("./routers");
-const cors = require('cors')
+const cors = require("cors");
 const dbConnect = require("./config/dbConfig");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.static("uploads"));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 dbConnect();
+app.use(cors());
 app.use(router);
-app.use(cors())
 
 app.listen(process.env.SERVER_PORT || 8080, () => {
   console.log("Server is running");
